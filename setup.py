e39347d471f78b0a49962d830aa7ea981713ef52
Derrick Petzold <contact@derrickpetzold.com>
2012-01-09 19:58:45 -1000

    Removed tests.

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
 from distutils.core import setup
-from distutils.cmd import Command
-from unittest import TextTestRunner, TestLoader
-import tests
-
-cmdclasses = dict()
-
-class TestCommand(Command):
-    """Runs the unit tests for mymodule"""
-
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        loader = TestLoader()
-        t = TextTestRunner()
-        t.run(loader.loadTestsFromModule(tests))
-#        t.run(unittest.TestLoader().loadTestsFromTestCase(tests.SanityCheck))
-
-# 'test' is the parameter as it gets added to setup.py
-cmdclasses['test'] = TestCommand
 
 setup(
     name = "dago",
