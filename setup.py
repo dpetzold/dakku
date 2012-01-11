e26a780c72f501772d698371d1810964054e0353
Derrick Petzold <contact@derrickpetzold.com>
2012-01-10 18:24:58 -1000

    more renaming stuff.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = "dago",
-    packages = ["dago"],
+    name = "dakku",
+    packages = ["dakku"],
     version = "0.0.1",
     description = "Dago - A Python Collection",
     author = "Derrick Petzold",
-    author_email = "dago@derrickpetzold.com",
-    url = "http://derrickpetzold.com/dago",
-    download_url = "http://media.derrickpetzold.com/dago-0.0.1.tgz",
+    author_email = "dakku@derrickpetzold.com",
+    url = "http://derrickpetzold.com/dakku",
+    download_url = "http://media.derrickpetzold.com/dakku-0.0.1.tgz",
     keywords = ['django'],
 )
