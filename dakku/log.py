1a92142f6d105b79332ca5f941620edf368712d1
Derrick Petzold <contact@derrickpetzold.com>
2012-01-12 00:08:41 -1000

    Made more generic.

--- dakku/log.py
+++ dakku/log.py
@@ -6,6 +6,8 @@ from django.http import HttpRequest
 
 # http://stackoverflow.com/questions/384076/how-can-i-make-the-python-logging-output-to-be-colored
 
+_logger = logging.getLogger(__name__)
+
 class StripFormatter(logging.Formatter):
     def __init__(self, format=None):
         logging.Formatter.__init__(self, format)
@@ -31,23 +33,25 @@ class RequestInfo(object):
         super(RequestInfo, self).__init__()
 
     def __getitem__(self, name):
-        if name == 'user.email':
-            if self.request.user.is_authenticated():
-                return self.request.user.email
-            return '<anonymous>'
-        elif name == 'path':
-            return self.request.path
-        elif name == 'session_key':
-            return self.request.session.session_key
-        else:
-            return '<?>'
+        _logger.info(name)
+        if not name.startswith('request.'):
+            return None
+        return eval('self.%s' % (name))
+
+    def _get_attrs(self, obj):
+        return [ a for a in dir(obj) if not a.startswith('_') and not callable(a) ]
 
     def __iter__(self):
-        keys = ['path', 'user.email', 'session_key']
+        keys = []
+        keys.extend(['request.%s' % (a) for a in self._get_attrs(self.request)])
+        keys.extend(['request.session.%s' % (a) for a in
+            self._get_attrs(self.request.session)])
+        keys.extend(['request.user.%s' % (a) for a in
+            self._get_attrs(self.request.user)])
         keys.extend(self.__dict__.keys())
         return keys.__iter__()
 
-def logger(name='crowdtube.views'):
+def logger(name):
     def wrap(func):
         def caller(*args, **kwargs):
             request = None
@@ -64,4 +68,3 @@ def logger(name='crowdtube.views'):
             return func(*args, **kwargs)
         return caller
     return wrap
-
